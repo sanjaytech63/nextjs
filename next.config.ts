e463233f9169effedb7c592cfd8ad1d85105bb77
y2code dev <y2codedev@gmail.com>
2025-01-20 16:34:28 +0530

    some changes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com', 'cdn.pixabay.com','cdn.rareblocks.xyz','pagedone.io','ucarecdn.com','cdn.sanity.io'],
   },
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
   /* config options here */
 };
 
